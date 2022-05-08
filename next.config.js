ccdfeb1ac6fadb29116e433bdca3f9214dfc8f58
Kapeesh Manilal <kapeesh@passionatepeople.io>
2022-05-08 15:58:47 +0200

    initial commit + testimonials page + contentful integration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['https://images.ctfassets.net']
+  },
   reactStrictMode: true,
 }
 
---
+++ pages/testimonials.tsx
@@ -0,0 +1,46 @@
+import * as contentful from 'contentful'
+import React from 'react'
+import Testimonials from '../components/Testimonials/Testimonials'
+import Header from '../components/Header/Header'
+import Head from 'next/head'
+
+
+export const TestimonialsPage = ({testimonials, mentees}) => {
+    return <>
+        <Head>
+            <title>Kapeesh.dev | Testimonials</title>
+            <meta name="description" content="What my colleagues and mentees have shared" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Header title={'TestimonialsMentorshipTestimonialsMentorship'} pageTitle={'Testimonials'} subtitle={'Learning Together'}/>
+        <Testimonials testimonials={ testimonials }/>
+
+        <Header title={'MenteesMenteesMenteesMenteesMentees'} pageTitle={'Mentees'} subtitle={''}/>
+        <Testimonials testimonials={ mentees }/>
+    </>
+}
+
+export async function getStaticProps() {
+    const client = contentful.createClient({
+                                               space: process.env.CONTENTFUL_SPACE_ID,
+                                               accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+                                           })
+
+    const testimonials = await client
+        .getEntries({content_type: 'testimonial'})
+        .then((response) => response.items);
+
+    const mentees = await client
+        .getEntries({content_type: 'mentees'})
+        .then((response) => response.items);
+    return {
+        props: {
+            testimonials,
+            mentees
+        }
+    }
+}
+
+
+export default TestimonialsPage
